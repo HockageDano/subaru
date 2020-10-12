79254c1d008eaf293e282185de5cccf4847ca91f
misha <misha2003rekhtman@gmail.com>
2020-10-12 22:36:38 +0300

    lab2.2

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -9,14 +9,14 @@ using namespace std;
 int main()
 {
 	double x; // вхідний параметр
-	double z1; // результат обчислення 1-го виразу
-	//double z2; // результат обчислення 2-го виразу
+	//double z1; // результат обчислення 1-го виразу
+	double z2; // результат обчислення 2-го виразу
 	cout << "x = "; cin >> x;
-	z1 = (sin(2.0 * x) + sin(5.0 * x) - sin(3.0 * x)) / (cos(x) + 1.0 - (2.0 * (sin(2.0 * x) * sin(2.0 * x))));
-	//z2 = 2.0 * sin(x);
+	//z1 = (sin(2.0 * x) + sin(5.0 * x) - sin(3.0 * x)) / (cos(x) + 1.0 - (2.0 * (sin(2.0 * x) * sin(2.0 * x))));
+	z2 = 2.0 * sin(x);
 	cout << endl;
-	cout << "z1 = " << z1 << endl;
-	//cout << "z2 = " << z2 << endl;
+	//cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
 	cin.get();
 	return 0;
 }
